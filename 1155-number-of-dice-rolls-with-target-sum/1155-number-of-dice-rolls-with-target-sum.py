1176e4bc4daf3b26a7cdc7d7773e8b7eafdf83c7
Venkateswarlu Kasireddy <85172370+venkateswarlu589@users.noreply.github.com>
2023-12-26 19:11:25 +0530

    Time: 236 ms (74.53%), Space: 23 MB (8.96%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+      mod = 10**9+7
+      @lru_cache(maxsize=None)
+      def func(i,t):
+        if t<0: return 0
+        elif i==n and t==0: return 1
+        elif i==n: return 0
+        return sum([func(i+1,t-x) for x in range(1,k+1)])
+      return func(0,target)%mod
